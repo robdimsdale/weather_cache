311e2ab2e0889c4818bc5a024f9e2fca6925009d
Rob Dimsdale-Zucker <robert.dimsdale@gmail.com>
2021-02-04 08:40:41 -0500

    Initial commit - cache weather

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_apscheduler import APScheduler
+import requests
+import os
+
+
+app = Flask(__name__)
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
+
+weather = {}
+
+
+# interval example
+@scheduler.task('interval', id='update weather', minutes=15)
+def update_weather():
+    print('Updating weather')
+    payload = {
+        'lat': os.getenv('LAT'),
+        'lon': os.getenv('LON'),
+        'appid': os.getenv('APP_ID'),
+        'units': os.getenv('UNITS', default='imperial')}
+    r = requests.get("https://api.openweathermap.org/data/2.5/weather?", params=payload)
+    if r.status_code != 200:
+        print('error updating weather')
+    else:
+        global weather
+        weather = r.text
+        print('Weather updated')
+
+
+@app.route('/')
+def weather():
+    return weather
+
+
+update_weather()
